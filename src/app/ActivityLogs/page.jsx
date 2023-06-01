d2bf9251997c9b74e1a1ed19489987ec11b5e339
Saif-Ullah-akqa <saif.ullah@akqa.com>
2023-06-01 11:53:04 +0530

    Activity log screen added

--- src/app/ActivityLogs/page.jsx
+++ src/app/ActivityLogs/page.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
+import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import activityLog from "../../activityLog.json";
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
 
 const ActivityLogs = () => {
   return (
-    <div>ActivityLogs</div>
-  )
-}
+    <Box
+      sx={{
+        marginLeft: 5,
+        marginTop: 5,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Typography variant="h3">Activity Logs</Typography>
+      <TableContainer component={Paper} sx={{ marginTop: 2 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Timestamps</TableCell>
+              <TableCell>Workflows</TableCell>
+              <TableCell>Log</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {activityLog.map((logEntry, index) => (
+              <TableRow key={index}>
+                <TableCell>{logEntry.timestamp}</TableCell>
+                <TableCell>{logEntry.workflowNode}</TableCell>
+                <TableCell>{logEntry.log}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
 
-export default ActivityLogs
+export default ActivityLogs;

0e6a067593fa4130d12393656c3a8d12a850f8e8
Saif-Ullah-akqa <saif.ullah@akqa.com>
2023-06-05 19:51:35 +0530

    Updated main screen styling

--- src/components/InitialBlock/index.js
+++ src/components/InitialBlock/index.js
@@ -1,9 +1,16 @@
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 const MainScreen = () => {
   return (
-    <Typography variant='h2'>Welcome!</Typography>
+    <Box
+    sx={{
+      margin: 5,
+      display: "flex",
+      flexDirection: "column",
+    }}
+  >    <Typography variant='h2'>Welcome!</Typography>
+  </Box>
   )
 }
 
